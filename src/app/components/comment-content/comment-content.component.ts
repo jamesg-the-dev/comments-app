304d58736452f8dfb1ddbe65e9d9221a04ff289e
James Guerra <jamesguerra2008@gmail.com>
2024-06-13 00:44:21 +1000

    create comment content

--- src/app/components/comment-content/comment-content.component.ts
+++ src/app/components/comment-content/comment-content.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-comment-content',
   standalone: true,
   imports: [],
   templateUrl: './comment-content.component.html',
-  styleUrl: './comment-content.component.scss'
+  styleUrl: './comment-content.component.scss',
 })
 export class CommentContentComponent {
-
+  @Input() text: string;
 }
