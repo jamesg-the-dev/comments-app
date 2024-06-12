5b6c01919c901810f6d7aecc425491ca0c0a5eb0
James Guerra <jamesguerra2008@gmail.com>
2024-06-13 00:53:16 +1000

    mock multiple comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SentCommentComponent } from './components/sent-comment/sent-comment.component';
+import { ReplyBoxComponent } from './components/reply-box/reply-box.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SentCommentComponent],
+  imports: [RouterOutlet, SentCommentComponent, ReplyBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'comments-app';
+  comments = [1, 2, 3];
 }
